1eead2b6f05dff7faee34bc692c1220c44fc3b90
Aluno <aluno@MacBook-Pro-de-Aluno.local>
2019-03-01 20:24:56 -0400

    first Commit

---
+++ SomaDiminuiApp/MainPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SomaDiminuiApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int N = 0;
+
+        void Button_Soma(object sender, System.EventArgs e)
+        {
+
+            N++;
+            label_valor.Text = N.ToString();
+        }
+
+
+        void Button_Subtrai(object sender, System.EventArgs e)
+        {
+
+            N--;
+            label_valor.Text = N.ToString();
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
